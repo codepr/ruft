8d32c75d2de861e4c5a4b3e6fc9c7ab0b2aeee30
Andrea <a.g.baldan@gmail.com>
2021-03-27 23:05:48 +0000

    Add raftcluster support rpc struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 mod raftmachine;
+use std::io;
+use std::sync::Arc;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let cluster = raftmachine::RaftCluster::new();
+    raftmachine::run(Arc::new(cluster), "127.0.0.1:8989").await?;
+    Ok(())
 }
